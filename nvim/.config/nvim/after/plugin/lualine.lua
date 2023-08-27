3ccfb59663eb0f7cec0537b10a538bbed5d09cb2
Fiquell <fiquellh@gmail.com>
2023-08-28 04:49:15 +0800

    Remove all fonts

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -6,20 +6,17 @@ end
 
 lualine.setup({
   options = {
+    icons_enabled = false,
     theme = "tokyonight",
     component_separators = "",
-    section_separators = { left = "", right = "" },
+    section_separators = "",
   },
   sections = {
-    lualine_a = {
-      { "mode", separator = { left = "" } },
-    },
+    lualine_a = { "mode" },
     lualine_b = { "branch" },
     lualine_c = { "diff", "filename" },
     lualine_x = { "filesize", "encoding", "fileformat", "filetype" },
     lualine_y = { "progress" },
-    lualine_z = {
-      { "location", separator = { right = "" } },
-    },
+    lualine_z = { "location" },
   },
 })
