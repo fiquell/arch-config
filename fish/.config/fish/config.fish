32a29427b3c20aefd7eed7dd5eb7f42df1edb9a3
Fiquell <fiquellh@gmail.com>
2023-08-29 09:49:45 +0800

    Added fish shells

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,69 @@
+#!/usr/bin/env fish
+#
+# Fiquell's fish environment
+
+# Welcome message
+function fish_greeting
+  fm6000 --random --color random
+end
+
+# Prompt
+starship init fish | source
+
+# Aliases
+alias cat="bat --theme base16 --style changes,numbers"
+alias cs="xclip -selection clipboard"
+alias grep="grep --color=always"
+
+# Changed "ls" to "exa"
+alias ls="exa --all --long --color=always --group-directories-first"
+alias la="exa --all --color=always --group-directories-first"
+alias lt="exa --all --tree --color=always --group-directories-first"
+
+# Git related commands
+alias ga="git add"
+alias gb="git branch"
+alias gc="git commit"
+alias gd="git diff"
+alias gl="git lg"
+alias gs="git st"
+alias gpl="git pull"
+alias gps="git push"
+alias gco="git checkout"
+
+# Themes
+# - https://github.com/folke/tokyonight.nvim/raw/main/extras/fish/tokyonight_night.fish
+
+set -l foreground c0caf5
+set -l selection 283457
+set -l comment 565f89
+set -l red f7768e
+set -l orange ff9e64
+set -l yellow e0af68
+set -l green 9ece6a
+set -l purple 9d7cd8
+set -l cyan 7dcfff
+set -l pink bb9af7
+
+# Syntax Highlighting Colors
+set -g fish_color_normal $foreground
+set -g fish_color_command $cyan
+set -g fish_color_keyword $pink
+set -g fish_color_quote $yellow
+set -g fish_color_redirection $foreground
+set -g fish_color_end $orange
+set -g fish_color_error $red
+set -g fish_color_param $purple
+set -g fish_color_comment $comment
+set -g fish_color_selection --background=$selection
+set -g fish_color_search_match --background=$selection
+set -g fish_color_operator $green
+set -g fish_color_escape $pink
+set -g fish_color_autosuggestion $comment
+
+# Completion Pager Colors
+set -g fish_pager_color_progress $comment
+set -g fish_pager_color_prefix $cyan
+set -g fish_pager_color_completion $foreground
+set -g fish_pager_color_description $comment
+set -g fish_pager_color_selected_background --background=$selection
