e528460940e9639f0ba4b21416a33054103162da
Fiquell <fiquellh@gmail.com>
2023-09-11 00:10:51 +0800

    Remove duplicate environment variable

--- bash/.bashrc
+++ bash/.bashrc
@@ -2,25 +2,10 @@
 #
 # Fiquell's Bash shell environment
 
-# Set global environment variables
-export EDITOR="nvim"
-export MANPAGER="nvim +Man!"
-export FZF_DEFAULT_OPTS="--margin 20% --info inline-right --color 16,bg+:#1a1b26"
+# Prompt
+eval "$(starship init bash)"
 
-# Add custom paths to the PATH variable
-export PATH="$HOME/.cargo/bin:$HOME/.local/bin:$HOME/bin:$PATH"
-
-# Aliases
-alias cat="bat --theme base16 --style changes,numbers"
-alias cs="xclip -selection clipboard"
-alias grep="grep --color=always"
-
-# Use "exa" instead of "ls" with custom options
-alias ls="exa --all --long --color=always --group-directories-first"
-alias la="exa --all --color=always --group-directories-first"
-alias lt="exa --all --tree --color=always --group-directories-first"
-
-# Git aliases
+# Git related commands
 alias ga="git add"
 alias gb="git branch"
 alias gc="git commit"
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,9 +5,8 @@
 # Set global environment variables
 set -gx EDITOR "nvim"
 set -gx MANPAGER "nvim +Man!"
-set -gx FZF_DEFAULT_OPTS "--margin 20% --info inline-right --color 16,bg+:#1a1b26"
 
-# Add custom paths to the PATH variable
+# Add custom path to PATH variable
 set -gx PATH "$HOME/.cargo/bin" "$HOME/.local/bin" "$HOME/bin" $PATH
 
 # Welcome message
@@ -18,17 +17,7 @@ end
 # Prompt
 starship init fish | source
 
-# Aliases
-alias cat "bat --theme base16 --style changes,numbers"
-alias cs "xclip -selection clipboard"
-alias grep "grep --color=always"
-
-# Use "exa" instead of "ls" with custom options
-alias ls "exa --all --long --color=always --group-directories-first"
-alias la "exa --all --color=always --group-directories-first"
-alias lt "exa --all --tree --color=always --group-directories-first"
-
-# Git aliases
+# Git related commands
 alias ga "git add"
 alias gb "git branch"
 alias gc "git commit"
